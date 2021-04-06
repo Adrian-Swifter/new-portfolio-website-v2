3d2baae5ffdfdd3f5c27877db080f80fd927617d
adrian-swifter <milosffh@yahoo.com>
2021-04-06 21:05:27 +0200

    window on build issue fix

--- new-portfolio-website/src/useWindowDimensions.js
+++ new-portfolio-website/src/useWindowDimensions.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window
+  }
+
   return {
     width,
-    height
-  };
+    height,
+  }
 }
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
-  );
+  )
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions(getWindowDimensions())
     }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
-  return windowDimensions;
+  return windowDimensions
 }
