f6201e8911f7a9ed59f254dd0d443edef9355cbc
adrian-swifter <milosffh@yahoo.com>
2021-04-06 21:13:20 +0200

    window on build issue fix v3

--- new-portfolio-website/src/components/background.js
+++ new-portfolio-website/src/components/background.js
@@ -1,14 +1,17 @@
-import React from "react";
-import useWindowDimensions from '../useWindowDimensions'
+import React from "react"
+import useWindowDimensions from "../useWindowDimensions"
 
 export default function Background() {
-
-  const { height, width } = useWindowDimensions();
+  if (typeof window !== "undefined") {
+    const { height, width } = useWindowDimensions()
+  }
   return (
     <div className="bg-image-wrapper">
       <img
         className="bg-image"
-        src={`https://source.unsplash.com/random/${width < 769 ? '500x750': '1600x900'}`}
+        src={`https://source.unsplash.com/random/${
+          width < 769 ? "500x750" : "1600x900"
+        }`}
         alt=""
       />
     </div>
