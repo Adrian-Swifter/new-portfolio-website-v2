c19532371f1b92cca4f01b2cddf876bf7d0072ce
adrian-swifter <milosffh@yahoo.com>
2021-04-06 21:31:06 +0200

    window on build issue fix v5

--- new-portfolio-website/src/components/background.js
+++ new-portfolio-website/src/components/background.js
@@ -1,19 +1,19 @@
 import React from "react"
 import useWindowDimensions from "../useWindowDimensions"
-if (typeof window !== "undefined") {
-  export default function Background() {
-    const { height, width } = useWindowDimensions()
 
-    return (
-      <div className="bg-image-wrapper">
-        <img
-          className="bg-image"
-          src={`https://source.unsplash.com/random/${
-            width < 769 ? "500x750" : "1600x900"
-          }`}
-          alt=""
-        />
-      </div>
-    )
+export default function Background() {
+  if (typeof window !== "undefined") {
+    const { height, width } = useWindowDimensions()
   }
+  return (
+    <div className="bg-image-wrapper">
+      <img
+        className="bg-image"
+        src={`https://source.unsplash.com/random/${
+          (width!== 'undefined'?width:'') < 769 ? "500x750" : "1600x900"
+        }`}
+        alt=""
+      />
+    </div>
+  )
 }
--- new-portfolio-website/src/useWindowDimensions.js
+++ new-portfolio-website/src/useWindowDimensions.js
@@ -8,21 +8,20 @@ if (typeof window !== "undefined") {
       height,
     }
   }
+}
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
 
-  export default function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(
-      getWindowDimensions()
-    )
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
 
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions())
-      }
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
-      window.addEventListener("resize", handleResize)
-      return () => window.removeEventListener("resize", handleResize)
-    }, [])
-
-    return windowDimensions
-  }
+  return windowDimensions
 }
