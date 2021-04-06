0a0267ba3b9b15b14b0c5b7ddfe45e1ec1c2ac93
adrian-swifter <milosffh@yahoo.com>
2021-04-07 01:15:28 +0200

    added website in construction notice

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -5,6 +5,7 @@ export default function Main() {
     <main className="main">
       <h1 className="main-heading">Milos Draskovic</h1>
       <h3 className="secondary-heading">Developing Web for Frontends</h3>
+      <p style={{color: "red"}}>Website in Construction</p>
     </main>
   )
 }
