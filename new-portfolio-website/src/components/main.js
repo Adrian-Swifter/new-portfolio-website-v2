6172ca2f8d9debd2bbd09bb0dd1c6f8d89113a31
MilosDraskovic <milosffh@yahoo.com>
2021-10-14 01:37:40 +0000

    added CV on home

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -3,10 +3,7 @@ import React from "react"
 export default function Main() {
   return (
     <main className="main">
-      <iframe src="https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="500px"></iframe>
-      {/* <h1 className="main-heading">Milos Draskovic</h1>
-      <h3 className="secondary-heading">Developing Web for Frontends</h3>
-      <p style={{color: "red"}}>Website in Construction</p> */}
+      <iframe src="https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="100%"></iframe>
     </main>
   )
 }
--- new-portfolio-website/src/pages/index.js
+++ new-portfolio-website/src/pages/index.js
@@ -1,14 +1,10 @@
 import React from "react"
-import Background from "../components/background"
-import Header from "../components/header"
 import Main from "../components/main"
 
 export default function Home() {
   return (
     <div>
-      <Header className="header"/>
       <Main />
-      <Background />
     </div>
   )
 }
