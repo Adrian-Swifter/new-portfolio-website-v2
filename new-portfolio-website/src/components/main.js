ab8a6a4a05042400e8eec97c14f3075f40f72830
MilosDraskovic <milosffh@yahoo.com>
2021-10-14 01:30:06 +0000

    testing CV pdf add

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -3,9 +3,10 @@ import React from "react"
 export default function Main() {
   return (
     <main className="main">
-      <h1 className="main-heading">Milos Draskovic</h1>
+      <iframe src="https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="500px"></iframe>
+      {/* <h1 className="main-heading">Milos Draskovic</h1>
       <h3 className="secondary-heading">Developing Web for Frontends</h3>
-      <p style={{color: "red"}}>Website in Construction</p>
+      <p style={{color: "red"}}>Website in Construction</p> */}
     </main>
   )
 }
