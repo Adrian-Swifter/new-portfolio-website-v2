217e0b2bd2b0b6cb02c6d1a770818ac23e61f34d
MilosDraskovic <milosffh@yahoo.com>
2021-10-14 01:57:14 +0000

    added embed instead of iframe

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main() {
   return (
     <main className="main">
-      <iframe src="https://docs.google.com/viewerng/viewer?url=https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="100%"></iframe>
+      <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="100%"></embed>
     </main>
   )
 }
