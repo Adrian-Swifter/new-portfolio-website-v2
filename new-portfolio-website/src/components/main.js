68f9ec51956da3edbf8f82e56d44c6d0ff833326
MilosDraskovic <milosffh@yahoo.com>
2021-10-14 03:02:44 +0000

    experimenting with hosting pdf

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main() {
   return (
     <main className="main">
-      <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="100%"></embed>
+      <embed src="../../static/MilosDraskovicCV.pdf" width="100%" height="100%"></embed>
     </main>
   )
 }
