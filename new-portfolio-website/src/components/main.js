2dcde32d7d1f99a54bda4afdc5b9bbf4c4e71e41
MilosDraskovic <milosffh@yahoo.com>
2021-10-14 03:21:07 +0000

    added PDF Embed API

--- new-portfolio-website/src/components/main.js
+++ new-portfolio-website/src/components/main.js
@@ -1,9 +1,20 @@
-import React from "react"
-
-export default function Main() {
-  return (
-    <main className="main">
-      <embed src="../../static/MilosDraskovicCV.pdf" width="100%" height="100%"></embed>
-    </main>
-  )
+import React, { Component } from 'react';
+import ViewSDKClient from '../components/ViewSDKClient';
+  
+  class Main extends Component {
+      componentDidMount() {
+          const viewSDKClient = new ViewSDKClient();
+          viewSDKClient.ready().then(() => {
+              /* Invoke file preview */
+              /* By default the embed mode will be Full Window */
+              viewSDKClient.previewFile("pdf-div", {});
+          });
+      }
+  
+      render() {
+          return <div id="pdf-div" className="full-window-div"/>;
+      }
+  }
+  
+  export default Main;
 }
