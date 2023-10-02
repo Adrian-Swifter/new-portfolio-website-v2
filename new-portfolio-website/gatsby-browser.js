b548895751313acb50dac29aa40d6ed84bc01e22
adrian-swifter <milosffh@yahoo.com>
2023-10-02 17:25:22 +0200

    complete website  redesign

--- new-portfolio-website/gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import "@fontsource/zilla-slab"
-import "@fontsource/zilla-slab-highlight/700.css"
-import "./src/styles/global.css"
--- new-portfolio-website/src/components/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Main() {
-  return (
-    <main className="main">
-      <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://wrdprssifix.com/wp-content/uploads/2021/10/MilosDraskovicCV.pdf" width="100%" height="100%"></embed>
-    </main>
-  )
-}
